f8f1d4bf6ff863c21d4f83fe964366d7d1f1bc99
ArteilDev <79750313+ArteilDev@users.noreply.github.com>
2022-08-28 16:03:39 +0300

    Add Animation

--- js/script.js
+++ js/script.js
@@ -79,3 +79,25 @@ function changeLanguage () {
 
 changeLanguage();
 //------------------------------------------------
+
+//-----------Анимация появления-------------------
+
+function onEntry(entry) {
+	entry.forEach(change => {
+    	if (change.isIntersecting) {
+      		change.target.classList.add('element-show')
+    	};
+  	});
+};
+
+let options = {
+  	threshold: [0.5]
+};
+
+let observer = new IntersectionObserver(onEntry, options);
+
+let elements = document.querySelectorAll('.element-anim');
+
+for (let elm of elements) {
+	observer.observe(elm);
+}
