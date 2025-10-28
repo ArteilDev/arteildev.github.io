ad91b25e0b1f7930e3d92eaa160ec18abdd1bada
ArteilDev <arteildev@gmail.com>
2025-10-28 16:44:06 +0300

    update

--- Aperture/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-	let navMenu = document.getElementById('navMenu')
-    let navbar = document.getElementById('navbar-responsive')
-    
-    navMenu.onclick = function () {
-        navbar.style.display == 'none' ? navbar.style.display = 'flex' : navbar.style.display = 'none'
-    }
-});
